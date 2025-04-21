032569553801483a535ab8697605733340b14a84
Anmol Poudyal <anmolpoudyal@Anmols-MacBook-Air.local>
2025-04-21 10:38:31 +0545

    changes in the register as well as servlets-Anmol

--- src/main/java/com/autorent/model/User.java
+++ src/main/java/com/autorent/model/User.java
@@ -1,13 +1,14 @@
 package com.autorent.model;
 
 public class User {
+    public enum Role{user, admin}
     private int userId;
     private String firstName;
     private String lastName;
     private String email;
     private String password;
     private String phone;
-    private String role;
+    private Role role;
     private String createdAt;
 
     // Constructors, Getters & Setters
@@ -16,7 +17,8 @@ public class User {
     public User() {
     }
 
-    public User(String firstName, String lastName, String email, String password, String phone, String role, String createdAt) {
+    public User(int userId, String firstName, String lastName, String email, String password, String phone, Role role, String createdAt) {
+        this.userId = userId;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
@@ -26,8 +28,7 @@ public class User {
         this.createdAt = createdAt;
     }
 
-    public User(int userId, String firstName, String lastName, String email, String password, String phone, String role, String createdAt) {
-        this.userId = userId;
+    public User(String firstName, String lastName, String email, String password, String phone, Role role, String createdAt) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
@@ -41,60 +42,60 @@ public class User {
         return userId;
     }
 
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
     public String getFirstName() {
         return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getPhone() {
         return phone;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public void setPhone(String phone) {
         this.phone = phone;
     }
 
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
+    public void setRole(Role role) {
         this.role = role;
-    }
-
-    public String getCreatedAt() {
-        return createdAt;
     }
 
     public void setCreatedAt(String createdAt) {
